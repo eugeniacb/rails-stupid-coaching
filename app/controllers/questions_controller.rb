75fd9e44083db0279143d70adb022ea98a3bbd56
Eugenia Brandao <eugeniacb@gmail.com>
2021-10-19 18:24:50 -0300

    nice game on rails

---
+++ app/controllers/questions_controller.rb
@@ -0,0 +1,14 @@
+class QuestionsController < ApplicationController
+  def answer
+    @question = params[:question].capitalize
+
+    case @question
+    when 'I am going to work'
+      @answer = 'Great!'
+    when @question.end_with?('?')
+      @answer = 'Silly question, get dressed and go to work!'
+    else
+      @answer = "I don't care, get dressed and go to work!"
+    end
+  end
+end
